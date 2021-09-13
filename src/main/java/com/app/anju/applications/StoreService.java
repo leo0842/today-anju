d7a039122e9a03a3585c167986aee90980cb5189
leokim <kunga077@gmail.com>
2021-09-13 12:27:07 +0900

    :sparkles: 모든 가게 조회 API 생성

--- src/main/java/com/app/anju/applications/StoreService.java
+++ src/main/java/com/app/anju/applications/StoreService.java
@@ -1,8 +1,15 @@
 package com.app.anju.applications;
 
+import com.app.anju.domain.Food;
+import com.app.anju.domain.FoodDetail;
+import com.app.anju.domain.FoodDetailRepository;
 import com.app.anju.domain.Store;
-import com.app.anju.domain.StoreDto;
+import com.app.anju.domain.StoresDto;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
@@ -12,21 +19,29 @@ import org.springframework.stereotype.Service;
 public class StoreService {
 
   private final MongoTemplate mongoTemplate;
+  private final FoodDetailRepository foodDetailRepository;
 
-  public void insertStore(StoreDto storeDto) {
+  public void insertStore(StoresDto storesDto) {
 
-    Store store = Store.builder()
-        .name(storeDto.getName())
-        .longitude(storeDto.getLongitude())
-        .latitude(storeDto.getLatitude())
-        .point(storeDto.getPoint())
-        .visited(storeDto.isVisited())
-        .build();
-    mongoTemplate.insert(store);
+    Store store = mongoTemplate.insert(storesDto.toStore());
   }
 
-  public List<Store> getStores() {
+  public List<StoresDto> getStores() {
 
-    return mongoTemplate.findAll(Store.class);
+    List<Store> stores = mongoTemplate.findAll(Store.class);
+    List<FoodDetail> foodDetails = foodDetailRepository.findByStoreIdIn(stores.stream().map(Store::get_id).collect(Collectors.toList()));
+    Map<String, List<Food>> stringFoodMap = new HashMap<>();
+    foodDetails.forEach(foodDetail -> toMap(stringFoodMap, foodDetail));
+
+    return stores.stream().map(store -> new StoresDto(store, stringFoodMap.get(store.get_id()))).collect(Collectors.toList());
+  }
+
+  private void toMap(Map<String, List<Food>> stringFoodMap, FoodDetail foodDetail) {
+
+    if (stringFoodMap.containsKey(foodDetail.getStoreId())) {
+      stringFoodMap.get(foodDetail.getStoreId()).add(foodDetail.getFood());
+    } else {
+      stringFoodMap.put(foodDetail.getStoreId(), Collections.singletonList(foodDetail.getFood()));
+    }
   }
 }
